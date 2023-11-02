4ff4175d780de8726956271031aca20e7fd32a1b
PokornicSR01 <nemanja.pokornic02@gmail.com>
2023-11-02 20:15:20 +0100

    initial commit

---
+++ reservation/main.go
@@ -0,0 +1,13 @@
+package reservation
+
+import "os"
+
+func main() {
+	//Reading from environment, if not set we will default it to 8080.
+	//This allows flexibility in different environments (for eg. when running multiple docker api's and want to override the default port)
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+
+}
