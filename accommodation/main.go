2bb055a86aa80b25071270e2eb7c4dd2544543a9
BojanDavogic <bokidavogic@gmail.com>
2023-11-14 22:57:49 +0100

    Changed database
    Changed database from cassandra to mongo

--- accommodation/main.go
+++ accommodation/main.go
@@ -4,15 +4,12 @@ import (
 	"accommodation/data"
 	"accommodation/handlers"
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
-	"strconv"
 	"time"
 
-	gocql "github.com/gocql/gocql"
 	gorillaHandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
@@ -31,66 +28,13 @@ func main() {
 	logger := log.New(os.Stdout, "[accommodation-api] ", log.LstdFlags)
 	storeLogger := log.New(os.Stdout, "[accommodation-store] ", log.LstdFlags)
 
-	// Reading enviroment for Cassandra
-	db := os.Getenv("CASS_DB")
-	cassandraHost := os.Getenv("CASSANDRA_HOST")
-	cassandraPortStr := os.Getenv("CASSANDRA_PORT")
-	cassandraPort, err := strconv.Atoi(cassandraPortStr)
-	if err != nil {
-		logger.Fatalf("Failed to parse CASSANDRA_PORT: %v", err)
-	}
-	cassandraUser := os.Getenv("CASSANDRA_USER")
-	cassandraPassword := os.Getenv("CASSANDRA_PASSWORD")
-
-	// Initializing Cassandra session
-	cluster := gocql.NewCluster(db)
-	cluster.Keyspace = "system"
-
-	session, err := cluster.CreateSession()
-	if err != nil {
-		log.Fatalf("failed to create session: %v", err)
-	}
-
-	err = session.Query(
-		fmt.Sprintf(`CREATE KEYSPACE IF NOT EXISTS %s
-					WITH replication = {
-						'class' : 'SimpleStrategy',
-						'replication_factor' : %d
-					}`, "accommodation", 1)).Exec()
-	if err != nil {
-		session.Close()
-		log.Fatalf("failed to create keyspace: %v", err)
-	}
-
-	// Close the session after keyspace creation
-	session.Close()
-
-	cluster = gocql.NewCluster(cassandraHost)
-	cluster.Keyspace = "accommodation"
-	cluster.Port = cassandraPort
-	cluster.Authenticator = gocql.PasswordAuthenticator{
-		Username: cassandraUser,
-		Password: cassandraPassword,
-	}
-	cluster.Consistency = gocql.One
-	session, err = cluster.CreateSession()
-	if err != nil {
-		logger.Fatalf("Failed to create Cassandra session: %v", err)
-	} else {
-		defer session.Close()
-		logger.Println("Connected to Cassandra")
-	}
-
 	// Initializing repo for accommodations
-	store, err := data.NewAccommodationRepository(storeLogger, session)
+	store, err := data.NewAccommodationRepository(timeoutContext, storeLogger)
 	if err != nil {
 		logger.Fatal(err)
 	}
-	err = store.CreateAccommodationTable()
-	if err != nil {
-		logger.Fatal(err)
-	}
-	defer store.CloseSession()
+	defer store.Disconnect(timeoutContext)
+	store.Ping()
 
 	accommodationsHandler := handlers.NewAccommodationsHandler(logger, store)
 
@@ -103,7 +47,12 @@ func main() {
 	router.HandleFunc("/accommodation/{id}", accommodationsHandler.UpdateAccommodation).Methods("PUT")
 	router.HandleFunc("/accommodation/{id}", accommodationsHandler.DeleteAccommodation).Methods("DELETE")
 
-	cors := gorillaHandlers.CORS(gorillaHandlers.AllowedOrigins([]string{"*"}))
+	// CORS middleware
+	cors := gorillaHandlers.CORS(
+		gorillaHandlers.AllowedOrigins([]string{"*"}),
+		gorillaHandlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"}),
+		gorillaHandlers.AllowedHeaders([]string{"Content-Type"}),
+	)
 
 	server := http.Server{
 		Addr:         ":" + port,
